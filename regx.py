c6db50f3f1f4bd1f11a44753c4eec84313224244
Fredrik Mile <fredrik.mile@gmail.com>
2021-07-14 08:11:50 +0200

    Add search function and support for ^ and $.

--- regx.py
+++ regx.py
@@ -1,12 +1,20 @@
 def matchOne(pattern: str, text: str) -> bool:
     if not text: return True
     if not pattern: return False
-    if pattern == ".": return True
+    if pattern == '.': return True
     return pattern == text
 
 def match(pattern: str, text: str) -> bool:
     if pattern  == "": return True
+    if pattern == '$' and text == "": return True
     return matchOne(pattern[0], text[0]) and match(pattern[1:], text[1:])
+
+def search(pattern: str, text: str) -> bool:
+    if pattern[0] == '^':
+        return match(pattern[1:], text)
+    return True in [match(pattern, text[x:]) for x in range(len(text)) ]
+
+print(search("bc", "abcd"))
 
 print(matchOne('a', 'a'))
 print(matchOne('.', 'z'))
