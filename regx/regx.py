9b5978a96a0dcea71c39eda8f67d024b39ccea40
Fredrik Mile <fredrik.mile@gmail.com>
2021-07-22 07:32:23 +0200

    Add support for '?'

--- regx/regx.py
+++ regx/regx.py
@@ -8,19 +8,27 @@ def matchOne(pattern: str, text: str) -> bool:
     return pattern == text
 
 
+def matchQuestion(pattern: str, text: str) -> bool:
+    # If the char before ? is matched
+    if len(text) > 0 and matchOne(pattern[0], text[0]) and match(pattern[2:], text[1:]):
+        return True
+    # If the char before ? is not matched
+    return match(pattern[2:], text)
+
+
 def match(pattern: str, text: str) -> bool:
     if pattern == "":
         return True
     if pattern == "$" and text == "":
         return True
+    if len(pattern) > 1 and pattern[1] == "?":
+        return matchQuestion(pattern, text)
     return matchOne(pattern[0], text[0]) and match(pattern[1:], text[1:])
 
 
 def search(pattern: str, text: str) -> bool:
     if pattern[0] == "^":
         return match(pattern[1:], text)
+    if len(text) == 0:
+        return match(pattern, text)
     return True in [match(pattern, text[x:]) for x in range(len(text))]
-
-
-print(search("bc", "abcd"))
-print(match("a.c", "abc"))
--- test/test_regx.py
+++ test/test_regx.py
@@ -10,3 +10,26 @@ def test_matchOne_valid():
 def test_matchOne_invalid():
     assert not regx.matchOne("a", "b")
     assert not regx.matchOne("p", "")
+
+
+def test_search():
+    assert regx.search("bc", "abcd")
+    assert not regx.search("bc", "ac")
+    assert regx.search("bc", "abcd")
+
+
+def test_search_with_end_of_string():
+    assert regx.search("a$", "aa")
+    assert not regx.search("a$", "abaaaab")
+
+
+def test_search_with_start_of_string():
+    assert regx.search("^a", "ab")
+    assert not regx.search("^a", "baaaab")
+
+
+def test_search_with_question_mark():
+    assert regx.search("ab?c", "ac")
+    assert regx.search("ab?c", "abc")
+    assert regx.search("a?b?c?", "")
+    assert regx.search("ab?c", "abc")
